3be7e7fd2bd1ae6c2a66e7d9d37018124f84f101
daw2-perez23 <perezabellanalex@fpllefia.com>
2023-02-23 11:10:46 +0100

    Server produccion2

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+//Servir contenido estáticos
+app.use( express.static('public') )
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hola mundo en su respectiva ruta')
+})
+  
+app.get('*', (req, res) => {
+    res.sendFile( __dirname + '/public/404.html' )
+})
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
